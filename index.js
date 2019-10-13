2a897ea3f212779dd3aa612cbb214e50452160cb
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-10-13 23:27:51 +0100

    Initialise a default axios insance

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-}
-axios.defaults.baseURL = portal.API_URL + '/' + portal.ALIAS + '/' + portal.ACTIVITY_SLUG + '/' + portal.MODULE_INSTANCE_SLUG;
-axios.defaults.params = {};
+let params = {};
 if(portal.group !== null) {
-    axios.defaults.params['group_id'] = portal.group.id;
+    params['group_id'] = portal.group.id;
 }
 if(portal.role !== null) {
-    axios.defaults.params['role_id'] = portal.role.id;
-
+    params['role_id'] = portal.role.id;
 }
 
-export default axios;
+const axiosInstance = axios.create({
+    params: params,
+    baseURL: portal.API_URL + '/' + portal.ALIAS + '/' + portal.ACTIVITY_SLUG + '/' + portal.MODULE_INSTANCE_SLUG
+});
+
+axiosInstance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    axiosInstance.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
+
+export default axiosInstance;
